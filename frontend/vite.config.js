a50b72accc95a47f8b7b77fcca8e20edb2bf9df5
magui <kimof4@gmail.com>
2025-09-03 18:01:33 +0900

    main layout add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      //프론트에서 /api로 시작하는 요청을 스프링(8080) 으로 보냄
-      '/api': 'http://localhost:8080',
+      // 예: /api, /ping 등 백엔드로 프록시
+      '/ping': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
